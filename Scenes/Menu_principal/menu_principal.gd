fad3e84510200cbdbc136491423a4e57cd7460f6
Bernardo Sanchez <103523880+Welleh@users.noreply.github.com>
2024-08-10 15:06:38 -0300

    Menu v1
    Boton salir funcional

---
+++ Scenes/Menu_principal/menu_principal.gd
@@ -0,0 +1,17 @@
+class_name MainMenu
+extends Control
+
+@onready var play_button = $"MarginContainer/HBoxContainer/VBoxContainer/Play Button" as Button
+@onready var exit_button = $"MarginContainer/HBoxContainer/VBoxContainer/Exit Button" as Button
+@onready var start_level = preload("res://Scenes/Test.tscn") as PackedScene #Especificar ruta de la escena principal
+
+func _ready():
+	play_button.button_down.connect(on_play_pressed)
+	exit_button.button_down.connect(on_exit_pressed)
+
+
+
+func on_play_pressed() -> void:
+	pass
+func on_exit_pressed() -> void:
+	get_tree().quit()
