8eed821dcd216c0853490ee46f227a33affa7382
Tinincho <martinsalvadorsosa@gmail.com>
2024-08-11 22:35:58 -0300

    Backup

--- Scripts/Enemy/script_state_enemy_death.gd
+++ Scripts/Enemy/script_state_enemy_death.gd
@@ -2,6 +2,22 @@ extends State
 
 func enter():
 	state_owner.animations.play("death")
+<<<<<<< Updated upstream
+=======
+	var my_random_number = rng.randi_range(0, 2)
+	
+	$"../../Punched".play()
+	
+	if (my_random_number == 0):
+		$"../../Death_01".play()
+	
+	if (my_random_number == 1):
+		$"../../Death_02".play()
+		
+	if (my_random_number == 2):
+		$"../../Death_03".play()
+	
+>>>>>>> Stashed changes
 
 func _on_animated_sprite_2d_animation_finished():
 	if owner.is_death == true:
